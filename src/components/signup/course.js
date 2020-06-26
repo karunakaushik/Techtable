4f21057eb1701d09d37b49458a20df4b7b2d5c33
Mohnish Lokhande <mohnishlokhande14@gmail.com>
2020-06-26 19:12:07 +0530

    new add

--- src/components/signup/course.js
+++ src/components/signup/course.js
@@ -9,40 +9,41 @@ import nine from './images/9th.svg';
 import ten from './images/10th.svg';
 import eleven from './images/11th.svg';
 import twelve from './images/12th.svg';
-
+import arrow from './images/Ellipse.svg';
+import sym from './images/Symbol.svg'
 
  
 const data=[
     {
-        title: "V grade",
+        title: "5",
         image: five
     },
     {
-        title:"VI grade",
+        title:"6",
         image:six
     },
     {
-        title:"VII grade",
+        title:"7",
         image: seven
     },
     {
-        title:"VIII grade",
+        title:"8",
         image: eigth
     },
     {
-        title:"IX grade",
+        title:"9",
         image: nine
     },
     {
-        title:"X grade",
+        title:"10",
         image: ten
     },
     {
-        title:"XI grade",
+        title:"11",
         image: eleven
     },
     {
-        title:"XII grade",
+        title:"12",
         image: twelve
     },
 ]
@@ -56,25 +57,27 @@ export default class Course extends Component{
     renderCard = (card,index)=>{
         return(
             <div className="container">
-                <Card className="courseCard gradeBox"key={index} style={{width:'150px', height:'150px'}} >
+                <Card className="courseCard gradeBox"key={index}  >
                     <Card.Img variant='top' src='holder.js/100px180' src={card.image} className="courseImage"/>
                 </Card>
-               <h3 onClick={this.handleChange } style={{backgroundColor:'#fff',border:'0px',color:'black'}}>{card.title}</h3>
+               <Button type="button" className="btn btn-link" onClick={this.handleChange } style={{backgroundColor:'#fff',border:'0px',color:'black'}}>{card.title} <sup>th</sup> Grade</Button>
             </div>
-        )
+        );
     
     }
 
 	render(){
 		
-		return <div>
-            <h2>Choose</h2>
-            <h1>Your Course</h1>
-            <div className='courseContainer'> {data.map(this.renderCard)} </div>
-            <br />
-            <Button style={{textAlign:'center' , width:"150px", backgroundColor: "#A34EAE"}}>CONTINUE</Button>
-        </div>
-
+		return( 
+            <div className="main">
+                <h2>Choose</h2>
+                <h1><b>Your Course</b></h1>
+                <br/>
+                <div className='courseContainer'> {data.map(this.renderCard)} </div>
+                <br />
+                <Button > CONTINUE</Button>
+            </div>
+        );
 	}
 	
 }
